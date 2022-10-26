e05bf8cd37e37f550519bb3e670b4817d178834e
miltonnotforyou <miltonnotforyou@yahoo.com.br>
2022-10-26 08:37:17 -0300

    atividades do curso java

---
+++ src/tiposVariaveis.java
@@ -0,0 +1,26 @@
+public class tiposVariaveis {
+    public static void main(String[] args) {
+        //tipos primitivos
+        // classe String que representa texto na aplicação
+
+        String meuNome = "Milton Nascimento";
+
+        double salarioMinimo = 2500;
+
+        // TiposEVariaveis.java
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 5;
+
+        numero = 10;
+
+        System.out.print(numero);
+
+        final double VALOR_DE_PI = 3.14;
+
+
+    }
+}
