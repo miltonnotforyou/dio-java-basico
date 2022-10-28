465267876483c2837182f32e43db277a68d212a7
miltonnotforyou <miltonnotforyou@yahoo.com.br>
2022-10-28 07:41:31 -0300

    atividades do curso java

--- src/classe_Operadores.java
+++ src/classe_Operadores.java
@@ -70,6 +70,21 @@ public class classe_Operadores {
 
            System.out.println("numero 1 é maior a numero 2?= " + simNao);
 
+        boolean condicao1=true;
+
+        boolean condicao2=false;
+
+        if(condicao1 && (7 > 4)){
+            System.out.println("As duas condições são verdadeiras");
+        }
+
+        if(condicao1 || condicao2) {
+            System.out.println("Uma das condições é verdadeira");
+        }
+
+        System.out.println("Fim");
+
+
     }
 
 }
