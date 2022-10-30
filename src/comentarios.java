57c6be772ac02a2d48d4974f5cb90490ab88356e
miltonnotforyou <miltonnotforyou@yahoo.com.br>
2022-10-30 12:58:00 -0300

    atividades do curso java

--- src/comentarios.java
+++ src/comentarios.java
@@ -15,5 +15,22 @@ public class comentarios {
          * a nível de documentação.
          * Que incrível !!!
          */
+
+        /* -> PRATICAS NÃO RECOMENDADAS:
+         * Este método foi elaborado as pressas
+         * por isso eu abrevei o nome das variáveis
+         * mas olha, ele tem a finalidade de somar ou  multiplicar
+         * dois números
+         * /
+        public int somaMultiplica (int n, int x, String m){
+            int r = 0; // r é igual ao resultado
+            if (m == "M"){ // M= multiplicação
+                r= n * x;
+            }else{
+                // se não soma mesmo
+                r = n + x
+            }
+            return r;
+}
     }
 }
