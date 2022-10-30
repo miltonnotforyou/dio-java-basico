ef48d41b988c3c70a277ad41d532b87e3464ba28
miltonnotforyou <miltonnotforyou@yahoo.com.br>
2022-10-30 12:55:21 -0300

    atividades do curso java

---
+++ src/comentarios.java
@@ -0,0 +1,19 @@
+public class comentarios {
+    public static void main(String[] args) {
+        // - > one line:  Olá, eu sou um comentário em uma única linha
+
+        /* - > Mult line:  Olá,
+         * Eu sou um comentario
+         * que posso ser mais detalhado
+         * quando necessário
+         */
+
+        /** - > Documentation:
+         * Estas duas estrelinhas acima
+         * é para identificar que você
+         * pretende elaborar um comentário
+         * a nível de documentação.
+         * Que incrível !!!
+         */
+    }
+}
